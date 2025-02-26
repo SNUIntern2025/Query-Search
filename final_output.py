55243f726449b21581dee706247c9185a432a95c
jihyeyu33 <jihyeyu33@hanyang.ac.kr>
2025-02-27 03:16:59 +0900

    [Fix] 함수 인자 수정정

--- final_output.py
+++ final_output.py
@@ -6,7 +6,7 @@ from query.query_with_gemma2 import special_tokens
 
 # TODO: 프롬프트 수정
 @timeit
-def final_output(query, subqueries, contexts, llm):
+def final_output(query, contexts, llm):
     model_name = getattr(llm, "model_name", getattr(llm, "model"))
     prompt = f"""{special_tokens[model_name]["system_start"]} 사용자의 질문에 답하세요.
         {special_tokens[model_name]["user_start"]} {{query}} {special_tokens[model_name]["end_token"]}
